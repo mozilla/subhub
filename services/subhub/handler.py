697bd376d215284d853b44e6d1c82021967aca46
Ben Bangert <ben@groovie.org>
2019-04-17 13:44:59 -0700

    feat: use awsgi for handler, requirement trims
    * Update the handler to use awsgi to return the Flask app and catch
      errors as needed.
    * Requirements trimmed down, test-requirements added, and tox updated
      to use them.

--- services/subhub/handler.py
+++ services/subhub/handler.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import logging
+import os
+import sys
 
-import json
+import awsgi
+
+# First some funky path manipulation so that we can work properly in
+# the AWS environment
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path)
 
 from subhub.app import create_app
 
+# Logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Create app at module scope to cache it for repeat requests
+try:
+    app = create_app()
+except Exception:  # pylint: disable=broad-except
+    logger.exception("Exception occurred while loading app")
+    # TODO: Add Sentry exception catch here
+    raise
+
+
 def handle(event, context):
     try:
-        app = create_app()
-        result = 'good to go!'
-    except Exception as ex:
-        result = str(ex)
-
-    body = {
-        "message": "well hello there buddy!",
-        "result": result,
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
+        return awsgi.response(app, event, context)
+    except Exception:  # pylint: disable=broad-except
+        logger.exception("Exception occurred while handling %s", event)
+        # TODO: Add Sentry exception catch here
