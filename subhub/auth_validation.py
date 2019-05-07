0ae208c3fbc84ca1f81484c187471ca916dac37f
Marty Ballard <marty331@gmail.com>
2019-05-07 14:39:13 -0500

    remove print statements

--- subhub/auth_validation.py
+++ subhub/auth_validation.py
@@ -6,9 +6,7 @@ from subhub.secrets import get_secret
 
 def payment_auth(api_token, required_scopes=None):
     secrets = json.loads(get_secret_values())
-    print(f'secrets {secrets}')
     if api_token in secrets:
-        print(f'token {api_token}')
         return {"token": api_token}
     return None
 
@@ -24,8 +22,6 @@ def get_secret_values():
 
 def support_auth(api_token, required_scopes=None):
     secrets = json.loads(get_support_values())
-    print(f'auth secrets {secrets}')
-    print(f'secrets type {type(secrets)}')
     if api_token in secrets:
         return {"token": api_token}
     return None
