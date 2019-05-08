bb0211417bb0f5d8ba4f62278b3d2773965b4c22
Scott Idler <scott.a.idler@gmail.com>
2019-05-08 12:56:35 -0700

    Fixup error handling Error message was being sent as a dict which was throwing another error.

--- subhub/exceptions.py
+++ subhub/exceptions.py
@@ -7,22 +7,46 @@ class SubHubError(Exception):
 class IntermittentError(SubHubError):
     status_code = 503
 
-    def __init__(self, message, status_code=None):
-        super().__init__(message)
-        self.status_code = status_code
+    def __init__(self, message, status_code=None, payload=None):
+        SubHubError.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
 
 
 class ClientError(SubHubError):
     status_code = 400
 
-    def __init__(self, message, status_code=None):
-        super().__init__(message)
-        self.status_code = status_code
+    def __init__(self, message, status_code=None, payload=None):
+        SubHubError.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
 
 
 class ServerError(SubHubError):
     status_code = 500
 
-    def __init__(self, message, status_code=None):
-        super().__init__(message)
-        self.status_code = status_code
+    def __init__(self, message, status_code=None, payload=None):
+        SubHubError.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
