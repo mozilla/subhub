37764a398358aa1683d75386625ee5651291320d
Scott Idler <scott.a.idler@gmail.com>
2019-05-08 08:57:33 -0700

    removed the need for the payment and support env vars

--- subhub/tests/unit/test_auth_validation.py
+++ subhub/tests/unit/test_auth_validation.py
@@ -1,15 +1,20 @@
+import os
+
 from subhub import auth_validation
 from subhub.cfg import CFG
 
+os.environ['PAYMENT_API_KEY'] = 'sh_payment_api_key'
+os.environ['SUPPORT_API_KEY'] = 'sh_support_api_key'
+
 
 def test_payment_auth():
-    api_token = "sh_test_testkey"
+    api_token = "sh_payment_api_key"
     payments_auth = auth_validation.payment_auth(api_token, required_scopes=None)
     assert payments_auth["value"] is True
 
 
 def test_payment_auth_bad_token():
-    api_token = "blahblah"
+    api_token = "sh_payment_api_key_bad"
     payments_auth = auth_validation.payment_auth(api_token, required_scopes=None)
     assert payments_auth is None
 
@@ -20,13 +25,13 @@ def test_get_secrets():
 
 
 def test_support_auth():
-    api_token = "sh_test_paykey"
+    api_token = "sh_support_api_key"
     support_auth = auth_validation.support_auth(api_token, None)
     assert support_auth["value"] is True
 
 
 def test_support_auth_bad_token():
-    api_token = "bad_paykey"
+    api_token = "sh_support_api_key_bad"
     support_auth = auth_validation.support_auth(api_token, None)
     assert support_auth is None
 
