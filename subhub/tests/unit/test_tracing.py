afb8af4ec26cbdf2ae2edaa11f0b63d460844f8a
Stewart Henderson <shenderson@mozilla.com>
2019-07-19 09:25:41 -0500

    Resolved a defect in the tracing decorators and added more unit test coverage

---
+++ subhub/tests/unit/test_tracing.py
@@ -0,0 +1,42 @@
+import functools
+
+from subhub.tracing import timed, mprofiled
+from random import randint, randrange
+
+
+@timed
+def generate_random_value():
+    return randint(0, 9)
+
+
+@timed
+def generate_wrapped_random_value():
+    return __wrapped(randint(0, 9))
+
+
+@timed
+def __wrapped(functor):
+    @functools.wraps(functor)
+    def call(*args, **kwargs):
+        return functor(*args, **kwargs)
+
+    return call
+
+
+@mprofiled
+def generate_random_list():
+    return [randrange(1, 101, 1) for _ in range(10)]
+
+
+def test_timed():
+    assert type(generate_random_value()) is int
+
+
+def test_timed_wrapped():
+    assert type(generate_wrapped_random_value()) is int
+
+
+def test_mprofile():
+    collection = generate_random_list()
+    assert isinstance(collection, list)
+    assert isinstance(collection[0], int)
