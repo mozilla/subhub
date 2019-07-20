8dafb352d4ce7e72314718d863bc01c0f9ee9933
Scott Idler <scott.a.idler@gmail.com>
2019-07-19 20:40:24 -0700

    added mpl2 header to the top of every .py file; added automation to check and apply

--- subhub/tests/behave/environment.py
+++ subhub/tests/behave/environment.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
 def before_scenario(context, scenario):
     """Seed empty HTTP headers to steps do not need to check and create."""
     context.headers = {}
