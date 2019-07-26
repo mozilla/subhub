0d29f8c1cf6abdcc4f623b0f12f61d974d2b94da
Jackie Munroe <jackiemunroe@users.noreply.github.com>
2019-07-26 09:42:53 -0700

    Updating tests to use mocks - Updated all test files to remove calls to Stripe - Updated all test files to use monkeypatch, mock or mockito

--- subhub/tests/unit/test_subhub.py
+++ subhub/tests/unit/test_subhub.py
@@ -3,7 +3,7 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import json
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, MagicMock
 import mock
 
 import connexion
@@ -11,6 +11,10 @@ import stripe.error
 
 from subhub.app import create_app
 from subhub.tests.unit.stripe.utils import MockSubhubUser
+
+from subhub.log import get_logger
+
+logger = get_logger()
 
 
 class MockCustomer:
@@ -35,6 +39,53 @@ def test_subhub():
     """
     app = create_app()
     assert isinstance(app, connexion.FlaskApp)
+
+
+def test_list_plans(app, monkeypatch):
+    """
+    GIVEN a valid token
+    WHEN a request for plans is made
+    THEN a success status of 200 is returned
+    """
+    client = app.app.test_client()
+
+    plans_data = [
+        {
+            "id": "plan_1",
+            "product": "prod_1",
+            "interval": "month",
+            "amount": 25,
+            "currency": "usd",
+            "nickname": "Plan 1",
+        },
+        {
+            "id": "plan_2",
+            "product": "prod_1",
+            "interval": "year",
+            "amount": 250,
+            "currency": "usd",
+            "nickname": "Plan 2",
+        },
+    ]
+
+    product_data = {"name": "Product 1"}
+
+    plans = Mock(return_value=plans_data)
+
+    product = Mock(return_value=product_data)
+
+    monkeypatch.setattr("stripe.Plan.list", plans)
+    monkeypatch.setattr("stripe.Product.retrieve", product)
+
+    path = "v1/plans"
+
+    response = client.get(
+        path,
+        headers={"Authorization": "fake_payment_api_key"},
+        content_type="application/json",
+    )
+
+    assert response.status_code == 200
 
 
 def test_update_customer_payment_server_stripe_error_with_params(app, monkeypatch):
@@ -113,6 +164,94 @@ def test_customer_signup_server_stripe_error_with_params(app, monkeypatch):
 
     assert response.status_code == 500
     assert "No such plan" in loaded_data["message"]
+
+
+def test_subscribe_success(app, monkeypatch):
+    """
+    GIVEN a route that attempts to make a subscribe a customer
+    WHEN valid data is provided
+    THEN a success status of 201 will be returned
+    """
+
+    client = app.app.test_client()
+
+    subscription_data = {
+        "id": "sub_123",
+        "status": "active",
+        "current_period_end": 1566833524,
+        "current_period_start": 1564155124,
+        "ended_at": None,
+        "plan": {"id": "plan_123", "nickname": "Monthly VPN Subscription"},
+        "cancel_at_period_end": False,
+    }
+
+    mock_false = Mock(return_value=False)
+
+    customer = Mock(return_value=MockCustomer())
+
+    customer_updated = MagicMock(
+        return_value={"id": "cust_123", "subscriptions": {"data": [subscription_data]}}
+    )
+
+    create = Mock(return_value={"id": "sub_234"})
+
+    user = Mock(return_value=MockSubhubUser())
+
+    monkeypatch.setattr("flask.g.subhub_account.get_user", user)
+    monkeypatch.setattr("stripe.Customer.retrieve", customer_updated)
+    monkeypatch.setattr("subhub.sub.payments.has_existing_plan", mock_false)
+    monkeypatch.setattr("subhub.sub.payments.existing_or_new_customer", customer)
+    monkeypatch.setattr("stripe.Subscription.create", create)
+
+    path = "v1/customer/subtest/subscriptions"
+    data = {
+        "pmt_token": "tok_visa",
+        "plan_id": "plan",
+        "origin_system": "fake_origin1",
+        "email": "subtest@example.com",
+        "display_name": "John Tester",
+    }
+
+    response = client.post(
+        path,
+        headers={"Authorization": "fake_payment_api_key"},
+        data=json.dumps(data),
+        content_type="application/json",
+    )
+    logger.info("response data", data=response.data)
+    assert response.status_code == 201
+
+
+def test_subscribe_customer_existing(app, monkeypatch):
+    """
+    GIVEN a route that attempts to make a subscribe a customer
+    WHEN the customer already exists
+    THEN an error status of 409 will be returned
+    """
+
+    client = app.app.test_client()
+
+    mock_true = Mock(return_value=True)
+
+    monkeypatch.setattr("subhub.sub.payments.has_existing_plan", mock_true)
+
+    path = "v1/customer/subtest/subscriptions"
+    data = {
+        "pmt_token": "tok_visa",
+        "plan_id": "plan",
+        "origin_system": "Test_system",
+        "email": "subtest@example.com",
+        "display_name": "John Tester",
+    }
+
+    response = client.post(
+        path,
+        headers={"Authorization": "fake_payment_api_key"},
+        data=json.dumps(data),
+        content_type="application/json",
+    )
+
+    assert response.status_code == 409
 
 
 def test_subscribe_card_declined_error_handler(app, monkeypatch):
