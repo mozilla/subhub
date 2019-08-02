06f9681e234c3b38c03be7cd10e715701fc6db89
Marty Ballard <marty331@gmail.com>
2019-08-02 13:04:50 -0500

    COPS-590 DeletedUsers - add SubHubDeletedAccount record - store user in SubHubDeletedAccount record - delete user record after move to SubHubDeletedAccount - add tests for new functionality

--- subhub/tests/conftest.py
+++ subhub/tests/conftest.py
@@ -78,6 +78,7 @@ def app():
     with app.app.app_context():
         g.subhub_account = app.app.subhub_account
         g.hub_table = app.app.hub_table
+        g.subhub_deleted_users = app.app.subhub_deleted
         yield app
 
 
