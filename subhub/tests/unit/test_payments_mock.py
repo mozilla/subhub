3caa31db73f3e4a26163fa083c0eec89f4070fe3
Marty Ballard <marty331@gmail.com>
2019-08-07 09:36:23 -0500

    COPS-327 - check_stripe_subscriptions - rename check_stripe_subscriptions to retrieve_stripe_subscriptions - raise AttributeError if subscriptions node does not exist - compare return value to submitted subscription id - validate cancel_at_period_end is True - update tests as appropriate

--- subhub/tests/unit/test_payments_mock.py
+++ subhub/tests/unit/test_payments_mock.py
@@ -6,6 +6,7 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import stripe
+import pytest
 
 from mockito import when, mock, unstub, ANY
 
@@ -15,7 +16,7 @@ from subhub.log import get_logger
 logger = get_logger()
 
 
-def test_check_stripe_subscriptions():
+def test_retrieve_stripe_subscriptions():
     response = mock(
         {
             "id": "cus_tester1",
@@ -25,14 +26,14 @@ def test_check_stripe_subscriptions():
         },
         spec=stripe.Customer,
     )
-    test_subscriptions = payments.check_stripe_subscriptions(response)
+    test_subscriptions = payments.retrieve_stripe_subscriptions(response)
     logger.info("test subscriptions", test_subscriptions=test_subscriptions)
     assert test_subscriptions[0]["id"] == "sub_123"
     assert test_subscriptions[0]["status"] == "active"
     unstub()
 
 
-def test_check_stripe_subscriptions_cancelled():
+def test_retrieve_stripe_subscriptions_cancelled():
     cancelled_sub = {"status": "cancelled", "id": "sub_124", "cancel_at": 232322}
 
     cancel_response = mock(
@@ -51,13 +52,13 @@ def test_check_stripe_subscriptions_cancelled():
     when(stripe.Customer).delete_source("cus_tester2", "src_123").thenReturn(
         delete_response
     )
-    test_cancel = payments.check_stripe_subscriptions(cancel_response)
+    test_cancel = payments.retrieve_stripe_subscriptions(cancel_response)
     logger.info("test cancel", test_cancel=test_cancel)
     assert test_cancel[0]["status"] == "cancelled"
     unstub()
 
 
-def test_check_stripe_subscriptions_fail():
+def test_retrieve_stripe_subscriptions_fail():
     cancel_response = mock(
         {
             "id": "cus_tester3",
@@ -73,25 +74,20 @@ def test_check_stripe_subscriptions_fail():
     when(stripe.Customer).delete_source("cus_tester3", "src_123").thenReturn(
         delete_response
     )
-    test_fail = payments.check_stripe_subscriptions(cancel_response)
+    test_fail = payments.retrieve_stripe_subscriptions(cancel_response)
     logger.info("test fail", test_fail=test_fail)
     assert test_fail == []
     unstub()
 
 
-def test_check_stripe_subscriptions_name_error():
+def test_retrieve_stripe_subscriptions_name_error():
     customer = mock(
         {
             "id": "cus_tester3",
             "object": "customer",
-            "subscriptions": {"data": []},
             "sources": {"data": [{"id": "src_123"}]},
         },
         spec=stripe.Customer,
     )
-    when(stripe.Customer).delete_source("cus_tester3", "src_123").thenRaise(NameError)
-
-    test_fail = payments.check_stripe_subscriptions(customer)
-    logger.info("test fail", test_fail=test_fail)
-    assert test_fail == []
-    unstub()
+    with pytest.raises(AttributeError):
+        payments.retrieve_stripe_subscriptions(customer)
