96ed19da8307ac5eccf9c419656f222f88c8241f
Stewart Henderson <shenderson@mozilla.com>
2019-08-17 09:23:26 -0500

    import fix

--- src/sub/tests/unit/test_deployed.py
+++ src/sub/tests/unit/test_deployed.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from sub.shared.cfg import CFG
+from shared.cfg import CFG
 from sub.shared.deployed import get_deployed
 
 
