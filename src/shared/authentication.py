71dc37fdddbd15f265906d0827aff510e96aaaa8
Jonathan Claudius <jclaudius@mozilla.com>
2019-09-12 13:15:59 -0400

    Make auth comparison less complex
    Factor out auth logic and make it more robust
    
    renamed common_auth -> test_token

--- src/shared/authentication.py
+++ src/shared/authentication.py
@@ -9,22 +9,27 @@ from shared.log import get_logger
 logger = get_logger()
 
 
-def payment_auth(api_token, required_scopes=None):
-    logger.info(f"api token {api_token}")
-    if api_token in (CFG.PAYMENT_API_KEY,):
+def test_token(test_api_token, cfg_api_token):
+    logger.info(f"test api token {test_api_token}")
+
+    # Make sure the config API token has a meaningful value set,
+    # to avoid an auth bypass on empty comparisons
+    if cfg_api_token in (None, "None", ""):
+        return None
+
+    if test_api_token == cfg_api_token:
         return {"value": True}
+
     return None
+
+
+def payment_auth(api_token, required_scopes=None):
+    return test_token(api_token, CFG.PAYMENT_API_KEY)
 
 
 def support_auth(api_token, required_scopes=None):
-    logger.info(f"api token {api_token}")
-    if api_token in (CFG.SUPPORT_API_KEY,):
-        return {"value": True}
-    return None
+    return test_token(api_token, CFG.SUPPORT_API_KEY)
 
 
 def hub_auth(api_token, required_scopes=None):
-    logger.info(f"api token {api_token}")
-    if api_token in (CFG.HUB_API_KEY,):
-        return {"value": True}
-    return None
+    return test_token(api_token, CFG.HUB_API_KEY)
