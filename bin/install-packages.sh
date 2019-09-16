95ac54c69bebe20e35bb1ba68b30f65ea6f64c15
Stewart Henderson <shenderson@mozilla.com>
2019-09-16 10:13:22 -0500

    Initial pinning of repo, Alpine version, and OpenSSL Dev version

--- bin/install-packages.sh
+++ bin/install-packages.sh
@@ -3,6 +3,7 @@
 # Alpine Registry for package versions, https://pkgs.alpinelinux.org/packages
 apk update
 
+ALPINE_VERSION=v3.7
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 IFS=$'\n' read -d '' -r -a lines < "${DIR}/../etc/alpine-packages"
-apk add --no-cache "${lines[@]}"
+apk add  --no-cache --update-cache --repository "http://nl.alpinelinux.org/alpine/${ALPINE_VERSION}/main" "${lines[@]}"
