1fee70326cc5cade54990b23520c7a0c2f8206d9
Marty Ballard <marty331@gmail.com>
2019-10-17 17:07:48 -0500

    Fix up containers - Add command to run sub as doit localsub - Add command to run hub as doit localhub - Update docs to reflect the change

--- src/sub/app.py
+++ src/sub/app.py
@@ -91,7 +91,7 @@ def create_app(config=None) -> connexion.FlaskApp:
     region = "localhost"
     host = f"http://localhost:{CFG.DYNALITE_PORT}"
     if is_docker():
-        host = f"http://dynamodb-local:{CFG.DYNALITE_PORT}"
+        host = f"http://dynamodb:{CFG.DYNALITE_PORT}"
     stripe.api_key = CFG.STRIPE_API_KEY
     logger.info("aws", aws=CFG.AWS_EXECUTION_ENV)
     if CFG.AWS_EXECUTION_ENV:
