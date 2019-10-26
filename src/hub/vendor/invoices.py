87dde0a91b52349375984fc1d16bc19d462cbd92
Jackie Munroe <jackiemunroe@users.noreply.github.com>
2019-10-25 17:25:45 -0700

    bug hub/vendor/invoices.py: filtered initial payment failures
    Added check to ensure invoice payment failure event notifications do not get sent if event is triggered on subscription creation.

--- src/hub/vendor/invoices.py
+++ src/hub/vendor/invoices.py
@@ -6,6 +6,7 @@ import json
 
 from stripe.error import InvalidRequestError
 from stripe import Product
+from typing import Dict, Any
 
 from hub.vendor.abstract import AbstractStripeHubEvent
 from hub.routes.static import StaticRoutes
@@ -16,19 +17,45 @@ logger = get_logger()
 
 
 class StripeInvoicePaymentFailed(AbstractStripeHubEvent):
-    def run(self) -> None:
+    def run(self) -> bool:
+        """
+        Handle Invoice Payment Failed events from Stripe.
+        If the payment failed on subscription_create take no action and :return false
+        Else format data to be sent to Salesforce :return true
+        :return:
+        """
+        logger.info("invoice payment failed event received", payload=self.payload)
+
+        if self.payload.data.object.billing_reason == "subscription_create":
+            logger.info(
+                "invoice payment failed on subscription create - data not sent to external routes"
+            )
+            return False
+
+        data = self.create_payload()
+        logger.info("invoice payment failed", data=data)
+        routes = [StaticRoutes.SALESFORCE_ROUTE]
+        self.send_to_routes(routes, json.dumps(data))
+        return True
+
+    def create_payload(self) -> Dict[str, Any]:
+        """
+        Create payload to be sent to external sources
+        :return payload:
+        :raises InvalidRequestError:
+        """
         try:
             invoice_data = self.payload.data.object.lines.data
-
             product = Product.retrieve(invoice_data[0]["plan"]["product"])
             nickname = format_plan_nickname(
                 product_name=product["name"],
                 plan_interval=invoice_data[0]["plan"]["interval"],
             )
         except InvalidRequestError as e:
+            logger.error("Unable to get plan nickname for payload", error=e)
             nickname = ""
-            logger.error("payment failed error", error=e)
-        data = self.create_data(
+
+        return self.create_data(
             customer_id=self.payload.data.object.customer,
             subscription_id=self.payload.data.object.subscription,
             currency=self.payload.data.object.currency,
@@ -37,6 +64,3 @@ class StripeInvoicePaymentFailed(AbstractStripeHubEvent):
             created=self.payload.data.object.created,
             nickname=nickname,
         )
-        logger.info("invoice payment failed", data=data)
-        routes = [StaticRoutes.SALESFORCE_ROUTE]
-        self.send_to_routes(routes, json.dumps(data))
