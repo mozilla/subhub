7799db9a8cdfe26f0af20193ad01281bef11a02f
Jackie Munroe <jackiemunroe@users.noreply.github.com>
2019-10-25 17:25:45 -0700

    bug hub/vendor/invoices.py: filtered initial payment failures
    Added check to ensure invoice payment failure event notifications do not get sent if event is triggered on subscription creation.

--- src/hub/tests/unit/stripe/invoice/test_stripe_invoice.py
+++ src/hub/tests/unit/stripe/invoice/test_stripe_invoice.py
@@ -2,52 +2,91 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import os
 import mock
+import unittest
 import json
-import mockito
-import requests
-import boto3
-import flask
 
-from hub.shared.cfg import CFG
-from hub.shared import secrets
-from hub.shared.tests.unit.utils import run_test, MockSqsClient
+from stripe.util import convert_to_stripe_object
+from stripe.error import InvalidRequestError
 
-CWD = os.path.realpath(os.path.dirname(__file__))
+from hub.vendor.invoices import StripeInvoicePaymentFailed
 
 
-def run_customer(mocker, data, filename):
-    # using pytest mock
-    mocker.patch.object(flask, "g")
-    flask.g.return_value = ""
+class StripeInvoicePaymentFailedTest(unittest.TestCase):
+    def setUp(self) -> None:
+        with open("tests/unit/fixtures/stripe_prod_test1.json") as fh:
+            prod_test1 = json.loads(fh.read())
+        self.product = convert_to_stripe_object(prod_test1)
 
-    run_test(filename, cwd=CWD)
+        with open("tests/unit/fixtures/stripe_in_payment_failed_event.json") as fh:
+            self.payment_failed_event = json.loads(fh.read())
 
+        with open(
+            "tests/unit/fixtures/stripe_in_payment_failed_event_sub_create.json"
+        ) as fh:
+            self.payment_failed_event_sub_create = json.loads(fh.read())
 
-@mock.patch("stripe.Product.retrieve")
-def test_stripe_invoice_payment_failed(mock_product, mocker):
-    fh = open("tests/unit/fixtures/stripe_prod_test1.json")
-    prod_test1 = json.loads(fh.read())
-    fh.close()
-    mock_product.return_value = prod_test1
+        product_patcher = mock.patch("stripe.Product.retrieve")
+        run_pipeline_patcher = mock.patch("hub.routes.pipeline.RoutesPipeline.run")
 
-    data = {
-        "event_id": "evt_00000000000000",
-        "event_type": "invoice.payment_failed",
-        "customer_id": "cus_00000000000",
-        "subscription_id": "sub_000000",
-        "currency": "usd",
-        "charge_id": "ch_000000",
-        "amount_due": 100,
-        "created": 1558624628,
-        "nickname": "Project Guardian (Daily)",
-    }
-    basket_url = CFG.SALESFORCE_BASKET_URI + CFG.BASKET_API_KEY
-    response = mockito.mock({"status_code": 200, "text": "Ok"}, spec=requests.Response)
-    mockito.when(boto3).client("sqs", region_name=CFG.AWS_REGION).thenReturn(
-        MockSqsClient
-    )
-    mockito.when(requests).post(basket_url, json=data).thenReturn(response)
-    filename = "invoice-payment-failed.json"
-    run_customer(mocker, data, filename)
+        self.addCleanup(product_patcher.stop)
+        self.addCleanup(run_pipeline_patcher.stop)
+
+        self.mock_product = product_patcher.start()
+        self.mock_run_pipeline = run_pipeline_patcher.start()
+
+    def test_run_success(self):
+        self.mock_product.return_value = self.product
+        self.mock_run_pipeline.return_value = None
+
+        did_run = StripeInvoicePaymentFailed(self.payment_failed_event).run()
+
+        assert did_run
+
+    def test_run_subscription_create(self):
+        self.mock_product.return_value = self.product
+        self.mock_run_pipeline.return_value = None
+
+        did_run = StripeInvoicePaymentFailed(self.payment_failed_event_sub_create).run()
+
+        assert did_run == False
+
+    def test_create_payload(self):
+        self.mock_product.return_value = self.product
+
+        expected_payload = {
+            "event_id": "evt_00000000000000",
+            "event_type": "invoice.payment_failed",
+            "customer_id": "cus_00000000000",
+            "subscription_id": "sub_000000",
+            "currency": "usd",
+            "charge_id": "ch_000000",
+            "amount_due": 100,
+            "created": 1558624628,
+            "nickname": "Project Guardian (Daily)",
+        }
+        actual_payload = StripeInvoicePaymentFailed(
+            self.payment_failed_event
+        ).create_payload()
+
+        assert expected_payload == actual_payload
+
+    def test_create_payload_nickname_error(self):
+        self.mock_product.side_effect = InvalidRequestError(message="", param="")
+
+        expected_payload = {
+            "event_id": "evt_00000000000000",
+            "event_type": "invoice.payment_failed",
+            "customer_id": "cus_00000000000",
+            "subscription_id": "sub_000000",
+            "currency": "usd",
+            "charge_id": "ch_000000",
+            "amount_due": 100,
+            "created": 1558624628,
+            "nickname": "",
+        }
+        actual_payload = StripeInvoicePaymentFailed(
+            self.payment_failed_event
+        ).create_payload()
+
+        assert expected_payload == actual_payload
