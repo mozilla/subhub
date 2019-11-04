6a4bad01001a25dc31e5a9cdb6132bcd35882393
Marty Ballard <marty331@gmail.com>
2019-11-04 12:50:24 -0600

    COPS-853 Improve code coverage on messages.py - Added test_messages.py to bring code coverage up to 100% for messages.py - Create new object MockClient for testing boto3 client

--- src/sub/messages.py
+++ src/sub/messages.py
@@ -3,13 +3,16 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import json
+
+from typing import Dict, Any
+from abc import ABC
+
 import boto3
 
-from abc import ABC
 from botocore.exceptions import ClientError
 
 from sub.shared.cfg import CFG
-from shared.log import get_logger
+from sub.shared.log import get_logger
 
 logger = get_logger()
 
@@ -18,25 +21,32 @@ class AbstractRoute(ABC):
     def __init__(self, payload) -> None:
         self.payload = payload
 
-    def report_route_error(self, payload) -> None:
-        logger.error("message route error", payload=payload)
-
 
 class Message(AbstractRoute):
-    def route(self) -> None:
+    def route(self) -> Dict[str, Any]:
+        sns_client = self.get_sns_client()
+        response = self.publish_sns_message(
+            sns_client=sns_client, topic_arn=CFG.TOPIC_ARN_KEY, message=self.payload
+        )
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            return response
+        return {"ResponseMetadata": {"HTTPStatusCode": 500}}
+
+    def get_sns_client(self):
+        return boto3.client("sns", region_name=CFG.AWS_REGION)
+
+    def publish_sns_message(
+        self, sns_client: boto3.client, topic_arn: str, message: Dict[str, Any]
+    ) -> Dict[str, Any]:
         try:
-            sns_client = boto3.client("sns", region_name=CFG.AWS_REGION)
             response = sns_client.publish(
-                TopicArn=CFG.TOPIC_ARN_KEY,
+                TopicArn=topic_arn,
                 Message=json.dumps(
-                    {"default": self.payload}
+                    {"default": message}
                 ),  # json.dumps is required by FxA
                 MessageStructure="json",
             )
-            if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
-                logger.info("message sent to queue", response=response)
-                logger.info("sns payload", payload=self.payload)
-                return response
+            return response
         except ClientError as e:
             logger.error("SNS error", error=e)
-            self.report_route_error(self.payload)
+            raise e
