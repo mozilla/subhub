78d631b8310b79074dc5349ef1092986ed239427
Marty Ballard <marty@mozilla.com>
2019-12-12 16:32:57 -0600

    COPS-852 Update sub/hub app.py tests - Updated tests for sub/app.py - Updated tests for hub/app.py - Added new method to handle GetError when no db connection exists

--- src/hub/tests/unit/test_app.py
+++ src/hub/tests/unit/test_app.py
@@ -1,17 +1,20 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import json
+from unittest import TestCase
+from mock import patch
 
 from flask import jsonify
 from stripe.error import AuthenticationError, CardError, StripeError
-from pynamodb.exceptions import GetError
+from hub.shared.exceptions import SubHubError
 
-from sub.app import create_app
-from sub.app import server_stripe_error
-from sub.app import intermittent_stripe_error
-from sub.app import server_stripe_error_with_params
-from sub.app import server_stripe_card_error
-from sub.app import database_connection_error
+from hub.app import create_app
+from hub.app import server_stripe_error
+from hub.app import intermittent_stripe_error
+from hub.app import server_stripe_error_with_params
+from hub.app import server_stripe_card_error
+from shared.cfg import CFG
 from shared.log import get_logger
 
 logger = get_logger()
@@ -20,6 +23,8 @@ logger = get_logger()
 def test_create_app():
     app = create_app()
     assert app
+    # subhub_error = app.display_subhub_errors("bad things")
+    print(f"subhub error {dir(app)} app= {app}")
 
 
 def test_intermittent_stripe_error():
@@ -57,13 +62,23 @@ def test_server_stripe_card_error():
     assert actual[1] == expected[1]
 
 
-def test_database_connection_error():
-    expected = (
-        jsonify({"message": "Server Error", "status_code": "bad_connection"}),
-        500,
-    )
-    error = GetError("Server Error")
-    actual = database_connection_error(error)
-    print(f"dce {actual}")
-    assert actual[0].json == expected[0].json
-    assert actual[1] == expected[1]
+class TestApp(TestCase):
+    def setUp(self) -> None:
+        self.app = create_app()
+        self.client = self.app.app.test_client()
+
+    def test_custom_404(self):
+        path = "/v1/versions"
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, 404)
+        # self.assertIn(path, response.data)
+        print(f"path {path} data {response}")
+
+    # def test_subhub_error(self):
+    #     with pytest.raises(SubHubError) as subhub_error:
+    #         expected = jsonify({"message": "something"}), 503
+    #         error = StripeError("something")
+    #         actual = intermittent_stripe_error(error)
+    #         assert actual[0].json == expected[0].json
+    #         assert actual[1] == expected[1]
+    #         assert actual[0]["status_code"] == expected[0]["status_code"]
