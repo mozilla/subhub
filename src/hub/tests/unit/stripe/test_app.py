f5c87f4ca61f4c250772ff7251136e6fce5f5aec
Marty Ballard <marty@mozilla.com>
2019-12-12 16:32:57 -0600

    COPS-852 Update sub/hub app.py tests - Updated tests for sub/app.py - Updated tests for hub/app.py - Added new method to handle GetError when no db connection exists

--- src/hub/tests/unit/stripe/test_app.py
+++ src/hub/tests/unit/stripe/test_app.py
@@ -4,17 +4,13 @@
 
 from flask import jsonify
 from stripe.error import AuthenticationError, CardError, StripeError
-from pynamodb.exceptions import GetError
 
-from sub.app import create_app
-from sub.app import server_stripe_error
-from sub.app import intermittent_stripe_error
-from sub.app import server_stripe_error_with_params
-from sub.app import server_stripe_card_error
-from sub.app import database_connection_error
-from shared.log import get_logger
-
-logger = get_logger()
+from hub.app import create_app
+from hub.app import server_stripe_error
+from hub.app import intermittent_stripe_error
+from hub.app import server_stripe_error_with_params
+from hub.app import server_stripe_card_error
+from shared.cfg import CFG
 
 
 def test_create_app():
@@ -53,17 +49,5 @@ def test_server_stripe_card_error():
     expected = jsonify({"message": "something", "code": "402"}), 402
     error = CardError("something", "param1", "402")
     actual = server_stripe_card_error(error)
-    assert actual[0].json == expected[0].json
-    assert actual[1] == expected[1]
-
-
-def test_database_connection_error():
-    expected = (
-        jsonify({"message": "Server Error", "status_code": "bad_connection"}),
-        500,
-    )
-    error = GetError("Server Error")
-    actual = database_connection_error(error)
-    print(f"dce {actual}")
     assert actual[0].json == expected[0].json
     assert actual[1] == expected[1]
