011bfd77beede13c5c8e4c4aa6aab8edd6ed563a
Stewart Henderson <shenderson@mozilla.com>
2020-03-30 19:42:24 -0500

    Use productname instead of plan_nickname

--- src/shared/tests/unit/test_utils.py
+++ src/shared/tests/unit/test_utils.py
@@ -3,34 +3,3 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 from hub.shared import utils
-
-
-def test_format_plan_nickname():
-    """
-    Given a product name and plan interval
-    Validate that interval is  formatted correctly
-    and the returned value is  concatenated correctly
-    :return:
-    """
-    product_name = "Test Product"
-    plan_interval = "month"
-    formatted_plan = utils.format_plan_nickname(
-        product_name=product_name, plan_interval=plan_interval
-    )
-    valid_plan_name = "Test Product (Monthly)"
-    assert valid_plan_name == formatted_plan
-
-
-def test_format_plan_key_error():
-    """
-    Given a product name and an invalid interval
-    Validate that the plan interval is not changed in the returned value
-    :return:
-    """
-    product_name = "Test Product"
-    plan_interval = "bi-weekly"
-    formatted_plan = utils.format_plan_nickname(
-        product_name=product_name, plan_interval=plan_interval
-    )
-    valid_plan_name = "Test Product (Bi-Weekly)"
-    assert valid_plan_name == formatted_plan
