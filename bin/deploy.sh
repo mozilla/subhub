dc841d5e776f1cf432f245d5fee5f1c306401bcc
Stewart Henderson <shenderson@mozilla.com>
2020-05-22 14:24:40 -0500

    Updated prod deployment script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,18 +5,11 @@ if [[ $TRAVIS_EVENT_TYPE == cron ]]; then
 fi
 
 case "$TRAVIS_BRANCH" in
-'feature/staging')
-    DEPLOY_ENV=staging
-    ;;
 'release/prod-test')
-    DEPLOY_ENV=prod-test
-    doit deploy
+    SKIP_TEST=true DEPLOYED_ENV=prod-test doit deploy
     ;;
 'release/prod')
-    SKIP_TESTS=true
-    DEPLOYED_ENV=prod
-    DEPLOY_ENV=prod
-    doit deploy
+    SKIP_TEST=true DEPLOYED_ENV=prod doit deploy
     ;;
 *)
     echo "No DEPLOY_ENV to set."
